551941f742141ae5b03c0ee802b4b69463fea9d0
khushii-13 <khushichaudhary904@gmail.com>
2025-02-04 14:06:23 +0530

    setup zippy

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const app = express();
+dotenv.config();
+const HOST = process.env.HOST;
+const PORT = process.env.PORT || 5000;
+const MONGO_URL = process.env.MONGO_URL;
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    console.log("DB is Connected");
+    app.listen(PORT, () => {
+      console.log(`Server is running at http://${HOST}:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello from Zippy");
+});
