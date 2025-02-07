c6bde7a3917d1256cc630895aae606364dad5ac0
khushii-13 <khushichaudhary904@gmail.com>
2025-02-07 10:53:43 +0530

    designed navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,21 @@
 import { Routes, Route } from "react-router-dom";
-import {Navbar} from "./components/Navbar";
-import {Login} from "./components/Login";
-import {Register} from "./components/Register";
-import {Home} from "./components/Home";
+import { Navbar } from "./components/Navbar";
+import { Login } from "./components/Login";
+import { Register } from "./components/Register";
+import { Home } from "./components/Home";
+import "./App.css"
 
 function App() {
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+      <div className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
     </>
   );
 }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,34 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaHome, FaUser, FaSignInAlt, FaUserPlus } from "react-icons/fa";
+
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="sidebar">
+      <div className="sidebar-logo">Zippy</div>
+      <ul className="sidebar-links">
+        <li>
+          <Link to="/">
+            <FaHome className="icon" /> Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/login">
+            <FaSignInAlt className="icon" /> Login
+          </Link>
+        </li>
+        <li>
+          <Link to="/register">
+            <FaUserPlus className="icon" /> Register
+          </Link>
+        </li>
+        <li>
+          <Link to="/profile">
+            <FaUser className="icon" /> Profile
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
